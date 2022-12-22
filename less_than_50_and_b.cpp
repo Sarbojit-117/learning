14a8c300ccc7ac493f3de98fa11a2051ec3b6f78
Sarbojit Bandyopadhyay <sarbojit-117@outlook.com>
2022-12-22 21:43:50 +0530

    Made some changes; deleted IDE's for actual learning.

---
+++ less_than_50_and_b.cpp
@@ -0,0 +1,14 @@
+//
+// Created by sarbo on 22-12-2022.
+//
+#include "iostream"
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter the value of a:";
+    cin>>a;
+    cout<<"Enter the value of b:";
+    cin>>b;
+    cout<<"The "
+}
